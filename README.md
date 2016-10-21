2d767f66733bf8ac7f83b3fe3e17388c6becacd9
Nathan Morin <nathanamorin@gmail.com>
2016-10-21 13:10:13 -0400

    organized cloud scripts & added file export method

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # cnitIoT
 CNIT 581 IoT Fall 2016 Data Analysis Scripts
 
-##Scripts
+
+##Cloud Scripts
 
 ###Door Sensor
-
+Python lib for extracting data from google sheets and into a python data structure.
--- cloudScripts/getDoorData.py
+++ cloudScripts/getDoorData.py
@@ -3,11 +3,6 @@ import csv
 
 GoogleSheetsURL = 'https://spreadsheets.google.com/feeds/download/spreadsheets/Export?key={key}&exportFormat=csv'
 
-#Dictionary of google sheets export urls
-DocumentKeys = {
-	'Wireless_Door_Lab':'147Gf1f3z-r1weIikSwSSd741baTqqqRkc06716gF09c',
-	'Presence':'1MAg21Z2H01UHSK1WiAzxfcqziD-CYvMnpafM_XPXwMs'
-}
 
 #Retrieves data from dict of urls & places CSV data into corresponding dict
 def getCSVData(urls):
@@ -17,12 +12,28 @@ def getCSVData(urls):
 
 	return csvData
 
+#Retrieves data from dict of urls & places CSV data into the specified file
+def writeCSVData(urls, fileURIs):
+	csvData = {}
+	for urlName in urls.keys():
+		with open(fileURIs[urlName],'w') as file:
+			file.write(urllib2.urlopen(GoogleSheetsURL.replace('{key}',urls[urlName])).read())
+
+
 
 
 
 #Example usage, downloads and prints data for the Wireless Door Lab
+#Dictionary of google sheets export urls
+DocumentKeys = {
+	'Wireless_Door_Lab':'147Gf1f3z-r1weIikSwSSd741baTqqqRkc06716gF09c'
+	# ,'Presence':'1MAg21Z2H01UHSK1WiAzxfcqziD-CYvMnpafM_XPXwMs'
+}
+
 CSVData = getCSVData(DocumentKeys)
 for event in CSVData['Wireless_Door_Lab']:
 	for field in event:
 		print field + '\t'
+
+writeCSVData(DocumentKeys,{'Wireless_Door_Lab':'testwireless.csv'})
 
